725f6cb8ce2c66263171ab65ee2483acf2e2c093
Jose-godoy-mendoza <jgodoym11@miumg.edu.gt>
2023-03-23 22:55:17 -0600

    Working_on_Sorting_Tokens

--- Compiladores/Compiladores.py
+++ Compiladores/Compiladores.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from pathlib import Path
 import re
+import numpy as np
+
 
 VentanaPrincipal = tk.Tk()
 VentanaPrincipal.title("Compiladores")
@@ -12,18 +14,18 @@ Frame = tk.Frame(VentanaPrincipal)
 Frame.pack()
 
 # ------------------- Upper design is shown as follow ------------------- #
-UpperLabelFrame = tk.LabelFrame(Frame, text="Label para frame superior")
+UpperLabelFrame = tk.LabelFrame(Frame, text="Analizer")
 UpperLabelFrame.grid(row=0, column=0, padx=10, pady=5)
 
 # Coding all left side of the frame
-LeftLabel = tk.Label(UpperLabelFrame, text = "Aun no definido")
+LeftLabel = tk.Label(UpperLabelFrame, text = "Text to be analized")
 LeftLabel.grid(row=0, column=0)
 
-LeftTextbox = tk.Text(UpperLabelFrame, width=40, height=30)
+LeftTextbox = tk.Text(UpperLabelFrame, width=65, height=30)
 LeftTextbox.grid(row=1, column=0)
 
 #Coding all right side of the frame 
-RightLabel = tk.Label(UpperLabelFrame, text = "tampoco está definido")
+RightLabel = tk.Label(UpperLabelFrame, text = "Tokens")
 RightLabel.grid(row=0, column=1)
 
 RightTextbox = tk.Text(UpperLabelFrame, width=65, height=30)
@@ -35,14 +37,14 @@ for widget in UpperLabelFrame.winfo_children():
 # ------------------- Upper design has been finished ------------------- #
 
 # ------------------- Lower desing is shown as follow ------------------- #
-LowerLabelFrame = tk.LabelFrame(Frame, text="Label para frame inferior")
+LowerLabelFrame = tk.LabelFrame(Frame, text="In this section we will see the errors on compilation time")
 LowerLabelFrame.grid(row=1, column=0, padx=10, pady=5)
 
 
-LowerLabel = tk.Label(LowerLabelFrame, text = "Titulo por defecto")
+LowerLabel = tk.Label(LowerLabelFrame, text = "Errors")
 LowerLabel.grid(row=0, column=0)
 
-LowerTextbox = tk.Text(LowerLabelFrame, width=105, height=10)
+LowerTextbox = tk.Text(LowerLabelFrame, width=115, height=10)
 LowerTextbox.grid(row=1, column=0)
 
 # The following code allows us to add the same padding to all the children contained in the LowerLabelFrame
@@ -64,7 +66,7 @@ def OpenFile():
     return File.name
 
 def SaveFile():
-    f = fd.asksaveasfile(mode='w', defaultextension=".txt")
+    f = fd.askMatrixasfile(mode='w', defaultextension=".txt")
     if f is None: 
         return
     f.write(LeftTextbox.get(1.0, 'end-1c'))
@@ -72,30 +74,83 @@ def SaveFile():
 # ------------------- End of file management ------------------- #
 
 # ------------------- Compile Function ------------------- #
+
+def Clasify_tokens(Matrix):
+    #print(len(Matrix))
+    print("1--------------------------------------1")
+    Operators = (["(",")","=","+", "-", "/", "*" ,"[", "]", "!=", "==", "<", ">", "<=", ">=", "&&", "||","{","}"])
+    Reserved_Words = (["if","else","else if","for","while","switch","private","return","void","int","string","double","float"])
+    Columns = 0
+    for Lines in Matrix:
+        Rows = 0
+        for Row in Lines:
+            #If print Lines it prints the whole line as much as values in that line
+            #print(Row) 
+            if Row in Operators:
+                Matrix[Columns][Rows] = "OP( "+Row+" )"
+                #print(Matrix[Columns][Rows])    #POSITION OF THE VALUE TO BE REPLACED
+                #print("Columna: ",  Columns,  "  Fila: ",  Rows)
+            elif Row in Reserved_Words:
+                Matrix[Columns][Rows] = "PR( "+Row+" )"
+            elif Row.isdigit():
+                Matrix[Columns][Rows] = "NUM( "+Row+" )"
+            elif Row !="":
+                Matrix[Columns][Rows] = "ID( "+Row+" )"
+            Rows = Rows + 1
+        RightTextbox.insert('end-1c', ",".join(Matrix[Columns]))
+        RightTextbox.insert('end-1c', "\n")
+        Columns = Columns + 1
+    return Matrix
+    #print(Matrix)
+
 def Compile():
     Tokens =""
+    Columns = 0
+    Matrix = [[]]
+    Operators = (["(",")","=","+", "-", "/", "*" ,"[", "]", "!=", "==", "<", ">", "<=", ">=", "&&", "||"])
     LeftText = LeftTextbox.get(1.0, 'end-1c')
     Text = LeftText.split("\n")
-    #RightTextbox.delete()
-    RightTextbox.insert("end-1c", len(Text))
-    Guardar = []
     for Lines in Text:
         position = 0
+        Row = 0
         Delimiter = len(Lines)-1
         while position <= Delimiter:
             Iteration = Lines[position]
-            if Iteration == " " or Iteration == "\t" or position==Delimiter:
-                Tokens = Tokens + Iteration
+            if (Iteration == " " or Iteration == "\t" or position==Delimiter) and (Iteration !="(" and Iteration !=")"):
+                if Iteration !=" " and Iteration !="\t":
+                    Tokens = Tokens + Iteration
                 if len(Tokens) >= 1: 
-                    Guardar.append(Tokens)  
+                    Matrix[Columns].append(Tokens) 
+                    Row = Row +1 
                     Tokens = ""
+            elif Iteration in Operators:
+                    if(Tokens!=""):
+                        Matrix[Columns].append(Tokens)
+                        Row = Row +1 
+                        Tokens = ""
+                    temp = Lines[position-1]
+                    print("*****************")
+                    if temp == Iteration:
+                        Matrix[Columns][Row]= Iteration + Iteration
+                        Row = Row +1 
+                    else:
+                        Matrix[Columns].append(Iteration)
             else:
                 Tokens = Tokens + Iteration
             position = position + 1
-        print(Guardar)
-    print("----------------------")
-    for a in Guardar:
-        print(a)        
+        Matrix.append([])
+        #print(Matrix)
+        #RightTextbox.insert('end-1c', ",".join(Matrix[Columns]))
+        #RightTextbox.insert('end-1c', "\n")
+        Columns = Columns + 1
+    Matrix = Clasify_tokens(Matrix)
+    print(Matrix)
+    
+
+
+
+
+     
 
 # ------------------- End of Compile Function ------------------- #
 
@@ -104,7 +159,7 @@ def Menu():
     VentanaPrincipal.config(menu=DropdownMenu)
     Opciones = tk.Menu(DropdownMenu)
     Opciones.add_command(label="Abrir Fichero", command=OpenFile)
-    Opciones.add_command(label="Guardar", command=SaveFile)
+    Opciones.add_command(label="Guardar Archivo", command=SaveFile)
     Opciones.add_command(label="Compilar Fichero", command=Compile)
     Opciones.add_command(label="Tabla de tokens")
     Opciones.add_separator()
@@ -115,17 +170,6 @@ Menu()
 VentanaPrincipal.mainloop()
 
 
-# ------------------- Lets obtain the text from the left TextBox ------------------- #
-def GetLeftText():
-    LeftText = LeftTextbox.get(1.0, 'end-1c')
-    Text = LeftText.split("\n")
-    #RightLabel.config(text=LeftText)
-    RightTextbox.insert("end-1c", len(Text))
-    
-# ------------------- Lets set the text from the rifht TextBox ------------------- #
-def SetRightText():
-    RightTextbox.delete(1.0, "end-1c")
-    RightTextbox.insert("end-1c","wenas")
 
 
 
--- Compiladores/Test.py
+++ Compiladores/Test.py
@@ -1,25 +1,54 @@
+def clasify_tokens(Matrix):
+    #print(len(Matrix))
+    Operators = (["(",")"])
+    print("1--------------------------------------1")
+    Columns = 0
+    for Lines in Matrix:
+        Rows = 0
+        for Row in Lines:
+            #If print Lines it prints the whole line as much as values in that line
+            #print(Row) 
+            if Row in Operators:
+                Matrix[Columns][Rows] = "OP"+Row
+                print(Matrix[Columns][Rows])    #POSITION OF THE VALUE TO REPLACE
+                print("Columna: ",  Columns,  "  Fila: ",  Rows)
+            if Row == "if" or Row =="else" or Row == "while" or Row == "for" or Row =="switch":
+                 Matrix[Columns][Rows] = "OP"+Row
+            Rows = Rows + 1
+        Columns = Columns + 1
+    print(Matrix)
+             
+
 Tokens =""
-Texto = "Li 1\nLis 2\npru"
+rows = 0
+columns = 0
+Texto = "col 1\n Linea 2(test) \n prueba 3"
 Text = Texto.splitlines()
-print (len(Text))
-Guardar = []
-#PosicionGuardar = 0
+Guardar = [[]]
 for Lines in Text:
         position = 0
+        #rows = 0
+        #columns = 0
         delimitador = len(Lines)-1
         while position <= delimitador:
             Iteration = Lines[position]
-            if Iteration == " " or Iteration == "\t" or position==delimitador:
-                Tokens = Tokens + Iteration
+            if (Iteration == " " or Iteration == "\t" or position==delimitador) and (Iteration !="(" and Iteration !=")"):
+                if Iteration !=" ":
+                    Tokens = Tokens + Iteration
                 if len(Tokens) >= 1: 
-                    Guardar.append(Tokens) 
+                    Guardar[columns].append(Tokens) 
                     Tokens = ""
+            elif Iteration == "(" or Iteration == ")":
+                    Guardar[columns].append(Tokens)
+                    Tokens = ""
+                    Guardar[columns].append(Iteration)
             else:
                 Tokens = Tokens + Iteration
             position += 1
+            #rows += rows
+        Guardar.append([])
+        columns = columns + 1
             #PosicionGuardar += PosicionGuardar
-        print(Guardar)
+clasify_tokens(Guardar)
 
-print("----------------------")
-for a in Guardar:
-     print(a)
+
